108130fbb686c8d368324951849678bb207ccfa2
彭晟赟 <shengyun@192.168.0.112>
2020-09-05 14:50:57 +0800

    Site updated: 2020-09-05 14:50:56

---
+++ assets/example.js
@@ -0,0 +1,29 @@
+// function popUp(winURL) {
+//     window.open(winURL, "popup", "width=800, height=480");
+// }
+//
+// // popUp("https://www.baidu.com")
+// // window.onload = prepareLinks()
+// function prepareLinks() {
+//     var links = document.getElementsByTagName("a");
+//     for (var i = 0; i < links.length; ++i) {
+//         if (links[i].getAttribute("class") == "popup") {
+//             links[i].onclick = function () {
+//                 popUp(this.getAttribute("href"));
+//                 return false;
+//             }
+//         }
+//     }
+// }
+
+// window.onload = function () {
+//     var para = document.createElement("p");
+//     var testdiv = document.getElementById("testdiv");
+//     testdiv.appendChild(para);
+//     var txt = document.createTextNode("Hello world!");
+//     para.appendChild(txt);
+//     alert(txt);
+// }
+alert("Hello")
+
+
